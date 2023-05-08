cffeecb4ed4c7b6397503239100f6dd4ddbeeea7
vicente <matheus31102001@gmail.com>
2023-05-07 21:30:53 -0300

    algumas modificações

--- Desktop/player/script.js
+++ Desktop/player/script.js
@@ -1,8 +1,1 @@
-function Pessoa(nome, idade, sexo) {
-    this.idade = idade;
-    this.nome = nome;
-    this.sexo = sexo;
-    aniversario = (idade) => {
-         this.idade = idade + 1
-    }
-}
+alert('bem vindo!')
